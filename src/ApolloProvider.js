f57d8b41de7e59812963b5f385c75c249630e232
ardgery <ardinegery@gmail.com>
2021-03-18 02:55:49 +0700

    proxy and uri

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -4,7 +4,7 @@ import { ApolloClient, InMemoryCache, createHttpLink, ApolloProvider } from '@ap
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000/'
+    uri: 'https://quiet-dusk-06833.herokuapp.com/'
 })
 
 const authLink = setContext((_, { headers }) => {
